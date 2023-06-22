55cc8990799b75a859d4016fc8f7cdd0d281482e
Sua <kimsuasch@gmail.com>
2023-06-22 14:33:24 +0900

    first commit try

---
+++ src/microwave/common.java
@@ -0,0 +1,27 @@
+package microwave;
+
+import java.util.Scanner;
+
+public class common {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		
+		while (true) {
+			
+			switch(n){
+			case 1: 
+				break;
+			
+			case 2: 
+				break;
+			
+			case 3: 
+				break;
+			
+			default: 
+				break;
+			}
+		}
+	}
+}
